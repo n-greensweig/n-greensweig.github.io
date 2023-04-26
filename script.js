2860f71cf6875293ed28254a1703eecb8f769921
n-greensweig <129970968+n-greensweig@users.noreply.github.com>
2023-04-26 16:16:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function typeWriter() {
 }
 
 typeWriter();
+
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+  if (!link.classList.contains('no-alert')) {
+    link.addEventListener('click', (event) => {
+      if (!confirm('Are you sure you want to leave this site?')) {
+        event.preventDefault();
+      }
+    });
+  }
+});
